4d8acf84123cbcd63521aef850f564471bd87e29
Cam McLeod <cammcleod3@gmail.com>
2019-05-28 12:36:48 -0700

    completed connecting everything task

--- Maths-Game/main.m
+++ Maths-Game/main.m
@@ -13,15 +13,27 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         while(TRUE) {
-            
+            //define char for user input
             char inputChars[255];
             
+            // initialize question which generates numbers
+            AdditionQuestion *newQuestion = [[AdditionQuestion alloc] init];
+            
+            //ask question
+            NSLog(@"%@", newQuestion.question);
+            
+            // take user input for answer
             fgets(inputChars, 255, stdin);
+            
+            // store string and trim as nsstring
             NSString *inputString = [NSString stringWithCString:inputChars encoding:NSUTF8StringEncoding];
             NSString *trimmedString = [inputString stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
             
-            NSLog(@"%@ newline?", inputString);
-            NSLog(@"%@ newline?", trimmedString);
+            if ([trimmedString integerValue] == newQuestion.answer) {
+                NSLog(@"Right!");
+            } else {
+                NSLog(@"Wrong! The right answer is: %ld", newQuestion.answer);
+            }
             
         }
     }
