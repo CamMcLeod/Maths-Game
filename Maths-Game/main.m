d12b83c6e2fed6be304352865cd039230fe6b937
Cam McLeod <cammcleod3@gmail.com>
2019-05-28 11:26:30 -0700

    Initial Commit

---
+++ Maths-Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths-Game
+//
+//  Created by Cameron Mcleod on 2019-05-28.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
